06242095179c0d2c3f748c592836af4e1bbb0c8f
claudia-rubio <68291719+claudia-rubio@users.noreply.github.com>
2021-10-13 21:27:11 -0400

    bootstrap and fa icons

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Vue2Editor from "vue2-editor";
-
+import "bootstrap";
+import "bootstrap/scss/bootstrap.scss";
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+library.add(faUserCircle);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(Vue2Editor);
 
 Vue.config.productionTip = false;
@@ -11,5 +17,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  //components: {"font-awesome-icon": FontAwesomeIcon},
   render: (h) => h(App),
 }).$mount("#app");
