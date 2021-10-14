c7a429b9e5b08047b2bc86f6ac08ca0319088eec
claudia-rubio <68291719+claudia-rubio@users.noreply.github.com>
2021-10-13 21:26:31 -0400

    add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="app-wrapper">
+    <nav-bar class="nav-bar"/>
     <div class="app">
       <router-view />
     </div>
@@ -7,9 +8,12 @@
 </template>
 
 <script>
+import NavBar from "./components/NavBar.vue";
 export default {
   name: "app",
-  components: {},
+  components: {
+    NavBar
+  },
   data() {
     return {};
   },
---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="./">Beach Avengers</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav col-md-11">
+                <li class="nav-item active">
+                    <a class="nav-link active" href="./login">Log In</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link active" href="./register">Register</a>
+                </li>
+            </ul>
+
+            <div class=" user col-md-1">
+                <a href="./profile">
+                    <font-awesome-icon icon="user-circle" size="2x"/>
+                </a>
+            </div>
+
+        </div>
+        </nav>
+</template>
+<style>
+    .navbar {
+        padding-left: 1rem;
+    }
+    li {
+        padding-left: 1rem;
+    }
+    .user {
+        align-content: right;
+    }
+
+</style>
