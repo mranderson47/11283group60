11fce38cfe65f0cf460e7e4dab9d417b6e4b1bf1
Mason Anderson <mason.csgotrading@gmail.com>
2021-10-16 00:22:16 -0400

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="left">
         <div class="col-1">
-          <router-link class="header" :to="{ name: 'Home' }">Med Binder</router-link>
+          <router-link class="header" :to="{ name: 'Home' }">Beach Avengers</router-link>
           <ul>
             <li>
               <a href="#"><youTube class="svg-icon"/></a>
