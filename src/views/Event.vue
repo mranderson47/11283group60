1b469a7f0f512f6b5f3ef28ab519da45751217b7
Kyle Helstrom <khelstrom@ufl.edu>
2021-10-17 18:50:48 -0400

    Added event form

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -41,8 +41,84 @@
                     ></button>
                 </div>
                 <div class="modal-body">
-                    <p> We'll add the form here</p>
+                    <!--
+                        Create event form
+                        -->
+
+                        <!--
+                    <div class="custom-file">
+                    <input type="file" class="custom-file-input" id="customFile">
+                    <label class="custom-file-label" for="customFile">Choose file</label>
+                    </div>
+                    -->
+
+                
+                <div class="form-group">
+                    <label for="exampleFormControlFile1">Cover Photo</label><br>
+                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                </div>
+                
+
+
+                    <form>
+
+                    <div class="form-group">
+                        <label for="eventName">Event Name</label>
+                        <input v-model= "event.name" type="text" class="form-control" id="eventNameInput" aria-describedby="eventName" placeholder="Enter event name">
+                        
+                    </div>
+
+                    <!--
+                        Insert date
+                        -->
+
+                    <div class="form-group">
+                        <label for="zipCode">Location Name</label>
+                        <input v-model= "event.zipCode" type="text" class="form-control" id="zipCode" placeholder="Enter zip code of location">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="eventLocation">Location Name</label>
+                        <input v-model= "event.locationName" type="text" class="form-control" id="locationName" placeholder="Enter name of location">
+                    </div>
+
+                    <div class="form-group">    
+                        <label for="">Google Maps Link of Location</label>
+                        <input v-model= "event.locationLink" type="url" class="form-control" id="locationLink" placeholder="Enter Google Maps link of location">
+                    </div>
+
+                    <div class="form-group">    
+                        <label for="">Purpose</label><br>
+                        <small id="purposeExplanation " class="form-text text-muted">What are you planning to do at this event?</small>
+                    <div class="form-horizontal">
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <textarea v-model= "event.purpose" class="form-control" rows="3" placeholder="Explain what will be done at this event" required></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
                     
+
+                    <!--
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+
+                    -->
+                    </form>
+
                 </div>
                 <div class="modal-footer">
                     <button
@@ -78,6 +154,7 @@ export default {
         return {
             searchKey: "",
             activeModal: {},
+            event: {name: "", locationName: "", locationLink: "", purpose: "", zipCode: ""}
         }
     },
     methods: {
@@ -106,6 +183,7 @@ export default {
         },
         saveEvent() {
             console.log("TODO: the event will be saved");
+            console.log(this.event);
             this.closeModal();
         }
     },
@@ -117,4 +195,41 @@ export default {
     width: 60%;
     margin-left: 40%;
 }
+
+.custom-file {
+    padding: 10px;
+    box-shadow: 10px 10px 20px #888888;
+
+}
+
+.modal-body {
+    padding: 30px;
+
+}
+
+.form-control {
+    padding: 5px;
+    box-shadow: 5px 5px 10px #888888;
+
+}
+
+.form-group {
+    padding: 5px;
+
+}
+
+form {
+    padding-top: 30px;
+
+}
+
+.form-control-file {
+    box-shadow: 5px 5px 10px #888888;
+
+}
+
+.form-horizontal {
+
+}
+
 </style>
