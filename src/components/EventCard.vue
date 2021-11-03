2e7f9521025d3b20c912af7d119535918e358689
claudia-rubio <68291719+claudia-rubio@users.noreply.github.com>
2021-11-02 20:46:20 -0400

    added event card component

---
+++ src/components/EventCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        {{event}}
+    </div>
+</template>
+<script>
+
+export default {
+    props: {
+        event: null
+    },
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
