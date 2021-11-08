c8d855a1cfb2816fd6a565ee8cbdd1023e67b972
claudia-rubio <68291719+claudia-rubio@users.noreply.github.com>
2021-11-08 17:48:57 -0500

    Updated UI

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,18 +1,20 @@
 <template>
 
-<div class="card w-75 hover-shadow" style="width: 18rem;">
-  <img class="card-img-top" src="https://clubsolaris.com/imgs/tips-to-take-care-of-the-beach-during-your-vacations/beach-sea-cancun-sun.png" alt="Card image cap"> <!-- Replace the src with the photo from the database for the event -->
-  <div class="card-body">
-    <span> <h2> Title: {{event.title}} </h2> </span>
-    <p class="card-text">
-    <span> <h4> Location: </h4> <p1>{{event.locationName}} </p1> </span>
-    <span> <h4> ZIP: </h4> <p1>{{event.zipcode}} </p1> </span>
-    <span> <h4> Date/Time: </h4> <p1>{{event.date}} </p1> </span>
-    <span> <h4> Google Maps Link: </h4> <p1>{{event.locationLink}} </p1> </span>
-    <span> <h4> Purpose: </h4> <p1>{{event.description}} </p1> </span>
-    </p>
-    
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="row card-event hover-shadow" >
+  <div class="col-md-5">
+    <img class="card-img-top" src="https://clubsolaris.com/imgs/tips-to-take-care-of-the-beach-during-your-vacations/beach-sea-cancun-sun.png" alt="Card image cap"> <!-- Replace the src with the photo from the database for the event -->
+  </div>
+  <div class="col-md-7 card-body">
+    <h4>{{event.title}}</h4> 
+    <span><b>Location: </b>{{event.locationName}} | <b>zip:</b> {{event.zipcode}} </span>
+    <br/>
+    <span> <b> Date/Time: </b> {{formattedDate}}  </span>
+    <br/>
+    <span v-if="event.locationLink"> <b> Google Maps Link: </b> {{event.locationLink}} </span>
+    <br v-if="event.locationLink"/>
+    <span v-if="event.description"> <b> Purpose: </b> {{event.description}} </span>
+    <br v-if="event.description"/>
+    <span> <b> By: </b> {{event.creatorName}} </span>
   </div>
 </div>
 
@@ -22,6 +24,22 @@
 export default {
     props: {
         event: null
+    },
+    computed: {
+        formattedDate() {
+            var date = new Date(this.event.date);
+            const year = date.getFullYear();
+
+            let month = (1 + date.getMonth()).toString();
+            month = month.length > 1 ? month : `0${month}`;
+
+            let day = date.getDate().toString();
+            day = day.length > 1 ? day : `0${day}`;
+            let hour = date.getHours();
+            let minute = date.getMinutes();
+
+            return `${month}/${day}/${year} ${hour}:${minute}`;
+        }
     },
     data() {
         return {
@@ -36,15 +54,15 @@ export default {
 }
 </script>
 <style scoped>
-    .card-container {
-        background-color: aqua;
 
-    }
+     .card-event {
+        margin: 2rem;
 
-     .card {
-        margin-top: 2rem;
-        margin-bottom: 2rem;
-        box-shadow: 10px 10px 20px #888888;
-
+        box-shadow: 10px 10px 20px #c5c5c5;
     } 
+    .card-img-top {
+        padding: 2px;
+        height: 100%;
+        width: 100%;
+    }
 </style>
