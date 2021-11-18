094f4ef903e0d9a2c670af1ac48960c6a9dbf4f7
Kyle Helstrom <khelstrom@ufl.edu>
2021-11-18 15:25:08 -0500

    Fixed settings button

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,64 +1,27 @@
 <template>
     <div class="profile-container">
-        <h1>Profile</h1>
-        <div class="container-sm setting-container">
-            <div class="accordion">
-                <div class="accordion-item">
-                <h2 class="accordion-header"> 
-                    <div class="d-flex">
-                    <button
-                        class="accordion-button collapsed "
-                        data-bs-toggle="collapse"
-                        data-bs-target="#settings"
-                        aria-expanded="false"
-                        aria-controls="settings"
-                        type="button"
-                    >
-                        Settings
-                    </button>
-                    <div class="d-flex align-items-start accordion-header-buttons">
-                    </div>
-                    </div>
-                </h2>
-                <div id="settings" class="accordion-collapse collapse">
-                    <p>Edit your user settings:</p>
-                    <form>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">First Name</label>
-                        <input type="text" class="form-control" id="firstName" v-model="firstName"/>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">Last Name</label>
-                        <input type="text" class="form-control" id="lastNae" v-model="lastName"/>
-                    </div>
-                    <br/>
-                    <div class="form-group">
-                        <button type="button" class="btn btn-primary" @click="updateProfile();">Save Settings</button>
-                    </div>
-                    </form>
-                </div>
-                </div>
-            </div> 
+        <div class="row">
+        <h1 class="col-md-11">Profile</h1>
+        <div class="form-group col-md-1 container-sm">
+                <button
+                    type="button"
+                    class="btn btn-success header-btn"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                    @click="openModal()"
+                >
+                Settings
+                </button>
+            </div>
         </div>
 
-
-<!-- <img src="https://cdn.mos.cms.futurecdn.net/wtqqnkYDYi2ifsWZVW2MT4-1200-80.jpg" class="rounded-circle" alt="Cinque Terre"> -->
 <img v-if="!profileImage" src="https://cdn.mos.cms.futurecdn.net/wtqqnkYDYi2ifsWZVW2MT4-1200-80.jpg" alt="Avatar">
 <img v-else :src="`${profileImage}`" alt="Avatar">
 
 
 <div class = "Name"> 
 <h2>{{$store.state.profileFirstName}} {{$store.state.profileLastName}}</h2>
-<!-- <h2>dasdasdfasdfasdfasdfasdfasdf  aa</h2> -->
 </div>
-
-    <button
-        type="button"
-        class="btn-close"
-        data-bs-dismiss="modal"
-        aria-label="Close"
-        @click="openModal()"
-    ></button>
 
 <div
         class="modal fade"
@@ -189,41 +152,8 @@
   <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...</div>
 </div>
 
-
-
         <!--Ending of tabs-->
-        
-<!--
 
-        <h2>Events you've created</h2>
-        <div class="row event-container">
-            <div class="col">
-                <div v-for="(event, index) in eventsFirstHalf" :key="index"> 
-                    <event-card :event="event" :key="event.id" />
-                </div>
-            </div>
-            <div class="col">
-                <div v-for="(event, index) in eventsSecondHalf" :key="index">
-                     <event-card :event="event" :key="event.id" />
-                </div>
-            </div>
-        </div>
-
-        <h2>Events you've liked</h2>
-        <div class="row event-container">
-            <div class="col">
-                <div v-for="(event, index) in likedEventsFirstHalf" :key="index"> 
-                    <event-card :event="event" :key="event.id" />
-                </div>
-            </div>
-            <div class="col">
-                <div v-for="(event, index) in likedEventsSecondHalf" :key="index">
-                     <event-card :event="event" :key="event.id" />
-                </div>
-            </div>
-        </div>
-
-        -->
 
     </div>
 </template>
@@ -363,10 +293,6 @@ export default {
 
         max-width: 20rem;
         max-height: 20rem;
-  
-        
-
-        /* width: 20rem; */
 
     }
 
